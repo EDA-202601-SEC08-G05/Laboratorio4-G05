493646bbca0255dd1ef21fe878a4dbe2fbdb560f
DWLastre <d.lastreo@uniandes.edu.co>
2026-02-15 10:34:56 -0500

    Avances funciones queue/stack

---
+++ DataStructures/Stack/stack.py
@@ -0,0 +1,34 @@
+from DataStructures.List import single_linked_list as lt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def is_empty(my_stack):
+    return lt.is_empty
+
+def top(my_stack):
+    if my_stack['size'] == 0:
+        raise Exception('EmptyStructureError: stack is empty')
+    
+    else:
+        t = lt.last_element
+        return t
+
+def size(my_stack):
+    sz = lt.size
+    return sz
