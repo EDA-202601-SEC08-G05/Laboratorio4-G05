44e054bf8e9a15cc0566225d9fb36b7860641fe1
sespinelg867 <s.espinelg2@uniandes.edu.co>
2026-02-17 08:13:50 -0500

    Primer Release

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -1,14 +1,12 @@
 from DataStructures.List import array_list as lt
 
 def new_queue():
-    newqueue = lt.new_list
-    
+    newqueue = lt.new_list()
     return newqueue
 
 def enqueue(my_queue, element):
-    
     lt.add_last(my_queue, element)
-
+    return my_queue
 
 def dequeue(my_queue):
     if my_queue['size'] == 0:
@@ -17,3 +15,17 @@ def dequeue(my_queue):
     else:
         first = lt.remove_first(my_queue)
         return first
+
+def is_empty(my_queue): 
+    return lt.is_empty(my_queue)
+
+def peek(my_queue): 
+    if is_empty(my_queue): 
+        raise Exception('EmptyStructureError: queue is empty')
+    
+    else: 
+        first = lt.first_element(my_queue)
+        return first
+    
+def size(my_queue): 
+    return lt.size(my_queue)
